f6daabdd40f7e3d42a81e4aa31a0a6acf2a913da
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-15 23:05:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driver,name){
+    return driver.filter(driver =>
+        driver.toLowerCase() === name.toLowerCase()
+    );
+}
+function fuzzyMatch(driver,name){
+   // return driver.filter(driver =>
+   //     driver.toLowerCase().startsWith(name.toLowerCase())
+   // );
+    return driver.filter(function(driver){
+        return driver.toLowerCase().startsWith(name.toLowerCase())
+    })
+}
+
+function matchName(driverObj,name){
+    return driverObj.filter(function(driverObj){
+        return driverObj.name===name
+    })
+}
